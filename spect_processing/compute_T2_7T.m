22d1d5584c0199f82cc91deefefcd2684c89e16e
romain valabregue <romain.valabregue@upmc.fr>
2016-04-14 10:29:14 +0200

    add a skip of TE value is CRLB above a hard define threshhold SD_thresh=50

--- spect_processing/compute_T2_7T.m
+++ spect_processing/compute_T2_7T.m
@@ -1,19 +1,22 @@
 %compute T2 from c result structure
-% d=get_subdir_regex('/servernas/images5/romain/spectro7T/LCmodel','T2_array','^[BCMO]','.*','no');
-%c=get_result(d)
+%select dir where lcmodel result are it assume a separate directory for
+%each subject (and each TE acquisition in this dir)
+d=get_subdir_regex('/servernas/images5/romain/spectro7T/LCmodel','T2_array','^[BCMO]','.*','no');
+c=get_result(d)
+
+SD_thresh = 50;   %parameter to skip te measure where CRLB abose SD_thresh
 
 metab_list = { 'Glu','Ins','sIns','sNAA','mNAA','GSHd','sNAA_NAAG','Glu_Gln','PCho_GPC','CrCH3_PCrCH3','CrCH2_PCrCH2','sNAA_mNAA','Tau'};
 
-figdir='/home/romain/images5/spectro7T/7T_MRS/T2_fit_figure';
-figdir='/home/romain/images5/spectro7T/7T_MRS/LCmodel/BG_spw2000/T2';
-figdir='/home/romain/images5/spectro7T/results/T2_new_array';
-figdir='/home/romain/images5/spectro7T/results/T2_all_fig2';
-figdir='/home/romain/images5/spectro7T/results/test';
-
+%choose output csv file
+figdir='/home/range4-raid1/mate/DATA/T2_fig';
 resfile = fullfile(figdir,'T2res.csv');
+
+%choose if saving the figure for the exponential fit
+save_fig=1;
+
 rf = fopen(resfile,'w+');
 
-save_fig=0;
 
 for nbsuj = 1:length(c)
 
@@ -25,11 +28,8 @@ for nbsuj = 1:length(c)
   for nte=1:length(c(nbsuj).suj)
     desc = c(nbsuj).suj{nte};
 
-    ind = findstr(desc,'TE_');
-    TEval = desc(ind+3:ind+5);
-    if TEval(end)=='_'
-      TEval(end)='';
-    end
+    ind = findstr(desc,'_TE_');
+    TEval = desc(ind+4:end);
 
     allte(nte) = str2num(TEval);
     fprintf(rf,' %d  ',allte(nte));
@@ -41,27 +41,37 @@ for nbsuj = 1:length(c)
   
   [te,ind]=sort(allte);
   allte=allte(ind);
-  if max(allte)>230
-    error('AZERAZERAZ')
+  if max(allte)>500
+    error('problem with find the TE')
   end
   
   for nmet = 1:length(metab_list)
     metc = getfield(c(nbsuj),metab_list{nmet});
+    metcSD =  getfield(c(nbsuj),['SD' metab_list{nmet}]);
     metc=metc(ind);
+    metcSD = metcSD(ind)
 
+    skip_te_ind = find(metcSD>SD_thresh);
+    if ~isempty(skip_te_ind)
+        fprintf('Warning skiping TE %d for %s',allte(skip_te_ind),metab_list{nmet});
+    end
     
-    [a,b,aa,bb,fit_error] = fit_ax_b(allte,log(metc));
+    curentte = allte; 
+    curentte(skip_te_ind) = [];
+    metc(skip_te_ind) = [];
+    
+    [a,b,aa,bb,fit_error] = fit_ax_b(curentte,log(metc));
 
     T2_lin = -1/a;
      
-    Rlin = log(metc) -  (a*allte+b) ;
+    Rlin = log(metc) -  (a*curentte+b) ;
      
     fit_error_lin = sqrt(sum(Rlin.*Rlin));
     
     vstart(1) = exp(b);    vstart(2) = T2_lin;
 
     try
-       [ vEnd,R,J,COVB,MSE] = nlinfit(allte,metc,@single_exp,vstart);
+       [ vEnd,R,J,COVB,MSE] = nlinfit(curentte,metc,@single_exp,vstart);
     catch
        R=1000000;
        vEnd=[1 1];
@@ -74,10 +84,10 @@ for nbsuj = 1:length(c)
     [vlin ilin]=max(rel_err_lin);
     [vexp iexp]=max(rel_err_exp);
    
-    titre_str = [ c(nbsuj).suj{1} '_' num2str(allte(end)) ' ' metab_list{nmet}];
+    titre_str = [ c(nbsuj).suj{1} '_' num2str(curentte(end)) ' ' metab_list{nmet}];
     
-    [R2_linfit P_linfit] = corrcoef(exp(a*allte+b),metc);
-    [R2_expfit P_expfit] = corrcoef(single_exp(vEnd,allte),metc);
+    [R2_linfit P_linfit] = corrcoef(exp(a*curentte+b),metc);
+    [R2_expfit P_expfit] = corrcoef(single_exp(vEnd,curentte),metc);
     
     R2_linfit = (R2_linfit(2)).^2;
     R2_expfit = (R2_expfit(2)).^2;
@@ -86,6 +96,7 @@ for nbsuj = 1:length(c)
     
     %CI = nlparci(vEnd,R,'jacobian',J);
     CI = nlparci(vEnd,R,'covar',COVB)
+% fprintf(rf,'metab,T2 lin, R2 lin,Pval, te of max error, max error , T2 exp, R2 exp, Pval,te of max error, max error,min_T2,max_T2,CI,CI/T2 \n');
 
 
     fprintf(rf,'%s, %0.1f, %f, %d,%0.3f, %0.1f, %0.1f, %f, %d,%0.3f, %0.1f, %0.1f, %0.1f, %0.1f, %f \n',metab_list{nmet},T2_lin,R2_linfit,P_linfit,allte(ilin),vlin*100,vEnd(2),R2_expfit,P_expfit,allte(iexp),vexp*100,CI(2,1),CI(2,2),CI(2,2)-CI(2,1),(CI(2,2)-CI(2,1))/vEnd(2));
@@ -97,23 +108,23 @@ for nbsuj = 1:length(c)
       h=figure;
    
       subplot(3,1,1);hold on
-      plot(allte,a*allte+b)
-      plot(allte,log(metc),'xr')
+      plot(curentte,a*curentte+b)
+      plot(curentte,log(metc),'xr')
       
       legend('linear fit log')
       title(titre_str)
       
       subplot(3,1,2);hold on
-      plot(allte,exp(a*allte+b))
-      plot(allte,metc,'xr')
+      plot(curentte,exp(a*curentte+b))
+      plot(curentte,metc,'xr')
       
       legend('linear fit')
     
       
       subplot(3,1,3);hold on
-      yy=single_exp(vEnd,allte);
-      plot(allte,yy)
-      plot(allte,metc,'xr')
+      yy=single_exp(vEnd,curentte);
+      plot(curentte,yy)
+      plot(curentte,metc,'xr')
       
       legend('exponential fit')
       
