a8c4a17a05638fe2f1a9ca390953435a98c2bbae
romain valabregue <romain.valabregue@upmc.fr>
2016-08-16 17:21:39 +0200

    specify a number of spectra to be average

--- spect_processing/average_fid.m
+++ spect_processing/average_fid.m
@@ -1,5 +1,18 @@
-function fi = average_fid(fi,descr)
+function fi = average_fid(fi,nb_spectra)
+
+if ~exist('nb_spectra')
+    for nbser = 1:length(fi)
+        nb_spectra(nbser) = size(fi(nbser).fid,2);
+    end
+end
+
+
+if length(fi) > length(nb_spectra)
+    nb_spectra=ones(size(fi))*nb_spectra;
+end
 
 for nbser = 1:length(fi)
-  fi(nbser).fid =sum( fi(nbser).fid,2)./(size(fi(nbser).fid,2));
+    
+    fi(nbser).fid =sum( fi(nbser).fid(:,1:nb_spectra(nbser)),2)./nb_spectra(nbser);
+    
 end
